9491cac7669bdec2ddfbb6068751a0dc13b32774
Jerry Wong <yuki.n@tuta.io>
2022-05-27 18:21:10 +0800

    add initial support for metamask

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './navbar.module.css'
 import Logo from '../../assets/fulllogo.svg'
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
 import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined'
-
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  SvgIcon,
+} from '@mui/material'
+import { useEthers } from '@usedapp/core'
+import MetamaskIcon from '../../assets/metamask-fox.svg'
+const WALLET_NOT_CONNECT = 'Not Connect'
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
+  const { activateBrowserWallet, deactivate, account } = useEthers()
+
+  const handleWalletDialog = () => {
+    open ? setOpen(false) : setOpen(true)
+  }
+
   return (
     <div className={classes.container}>
       <div className={classes.logoWrapper}>
@@ -19,9 +39,33 @@ const Navbar = () => {
           <AccountCircleOutlinedIcon style={{ height: '30px', width: '35px' }} />
         </div>
         <div className={classes.links}>
-          <AccountBalanceWalletOutlinedIcon style={{ height: '30px', width: '35px' }} />
+          <Button
+            variant="text"
+            startIcon={
+              <AccountBalanceWalletOutlinedIcon style={{ height: '30px', width: '35px' }} />
+            }
+            onClick={handleWalletDialog}
+          >
+            {account ?? WALLET_NOT_CONNECT}
+          </Button>
         </div>
       </div>
+      <Dialog onClose={handleWalletDialog} open={open}>
+        <DialogTitle>Wallet</DialogTitle>
+        <List>
+          <ListItem>
+            <ListItemButton
+              onClick={() => {
+                activateBrowserWallet()
+                account ?? setOpen(false)
+              }}
+            >
+              <ListItemIcon>{/* <MetamaskIcon /> */}</ListItemIcon>
+              Metamask
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Dialog>
     </div>
   )
 }
